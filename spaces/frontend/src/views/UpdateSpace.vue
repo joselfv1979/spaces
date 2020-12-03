e818eb5bab7cf9984adafd18a0e344a69197280b
joselfv1979 <61271903+joselfv1979@users.noreply.github.com>
2020-12-03 09:49:11 +0100

    Update UpdateSpace.vue

--- spaces/frontend/src/views/UpdateSpace.vue
+++ spaces/frontend/src/views/UpdateSpace.vue
@@ -1,131 +1,148 @@
 <template>
-  <div id="main">
+  <div class="container">
     <!-- TÍTULO PERSONALIZADO CON VUE-HEADFUL -->
     <vue-headful title="Espacios" />
-    <div>
-      <!-- <router-link :to="{ name: 'SpaceProviderList' }">
-        <button>Volver</button>
-      </router-link> -->
-      <div class="container">
-        <div class="form">
-          <h3>Actualiza tu espacio</h3>
-          <!-- INPUTS VINCULADOS AL DATA CON V-MODEL -->
-          <div class="data">
+    <div class="form">
+      <h3>Actualiza tu espacio</h3>
+      <!-- INPUTS VINCULADOS AL DATA CON V-MODEL -->
+      <div class="data">
+        <div>
+          <label for="name">Nombre del hotel</label>
+          <input
+            type="text"
+            v-model="hotel"
+            class="field"
+            placeholder="Hotel"
+          />
+        </div>
+        <div>
+          <label for="name">Ciudad</label>
+          <input
+            type="text"
+            v-model="city"
+            class="field"
+            placeholder="Ciudad"
+          />
+        </div>
+        <div>
+          <label for="name">Dirección</label>
+          <input
+            type="text"
+            v-model="address"
+            class="field"
+            placeholder="Dirección"
+          />
+        </div>
+        <div>
+          <label for="name">Nombre de la sala</label>
+          <input type="text" v-model="name" class="field" placeholder="Sala" />
+        </div>
+        <div>
+          <label for="name">Precio por hora</label>
+          <input
+            type="number"
+            v-model="price"
+            class="field"
+            placeholder="Precio"
+          />
+        </div>
+      </div>
+      <div class="bordered-box">
+        <div class="group">
+          <div class="options">
+            <h4>Disposición</h4>
             <div>
-              <label for="name">Nombre del hotel</label>
-              <input type="text" v-model="hotel" class="field" placeholder="Hotel" />
+              <label for="uLayout">
+                <input type="checkbox" v-model="uLayout" />En U
+              </label>
             </div>
             <div>
-              <label for="name">Ciudad</label>
-              <input type="text" v-model="city" class="field" placeholder="Ciudad" />
+              <label for="classLayout">
+                <input type="checkbox" v-model="classLayout" />En escuela
+              </label>
             </div>
             <div>
-              <label for="name">Dirección</label>
-              <input type="text" v-model="address" class="field" placeholder="Dirección" />
-            </div>
-            <div>
-              <label for="name">Nombre de la sala</label>
-              <input type="text" v-model="name" class="field" placeholder="Sala" />
-            </div>
-            <div>
-              <label for="name">Precio por hora</label>
-              <input type="number" v-model="price" class="field" placeholder="Precio" />
+              <label for="theaterLayout">
+                <input type="checkbox" v-model="theaterLayout" />En teatro
+              </label>
             </div>
           </div>
-          <div class="bordered-box">
-            <div class="group">
-              <div class="options">
-                <h4>Disposición</h4>
-                <div>
-                  <label for="uLayout">
-                    <input type="checkbox" v-model="uLayout" />En U
-                  </label>
-                </div>
-                <div>
-                  <label for="classLayout">
-                    <input type="checkbox" v-model="classLayout" />En escuela
-                  </label>
-                </div>
-                <div>
-                  <label for="theaterLayout">
-                    <input type="checkbox" v-model="theaterLayout" />En teatro
-                  </label>
-                </div>
-              </div>
-              <div class="options">
-                <h4>Tipo</h4>
-                <div>
-                  <label for="meetingType">
-                    <input type="checkbox" v-model="meetingType" />Reuniones
-                  </label>
-                </div>
-                <div>
-                  <label for="hallType">
-                    <input type="checkbox" v-model="hallType" />Eventos
-                  </label>
-                </div>
-              </div>
+          <div class="options">
+            <h4>Tipo</h4>
+            <div>
+              <label for="meetingType">
+                <input type="checkbox" v-model="meetingType" />Reuniones
+              </label>
+            </div>
+            <div>
+              <label for="hallType">
+                <input type="checkbox" v-model="hallType" />Eventos
+              </label>
             </div>
           </div>
-          <div class="bordered-box">
-            <div class="group">
-              <div class="options">
-                <h4>Apertura</h4>
-                <div class="hour">
-                  <input type="time" v-model="openTime" />
-                </div>
-                <h4>Cierre</h4>
-                <div class="hour">
-                  <input type="time" v-model="closeTime" />
-                </div>
-              </div>
-              <div class="options">
-                <h4>Equipamiento</h4>
-                <div>
-                  <label for="furniture">
-                    <input type="checkbox" v-model="furniture" />Muebles
-                  </label>
-                </div>
-                <div>
-                  <label for="wifi">
-                    <input type="checkbox" v-model="wifi" />WI-FI
-                  </label>
-                </div>
-                <div>
-                  <label for="projector">
-                    <input type="checkbox" v-model="projector" />Proyector
-                  </label>
-                </div>
-                <div>
-                  <label for="screen">
-                    <input type="checkbox" v-model="screen" />Pantalla
-                  </label>
-                </div>
-              </div>
+        </div>
+      </div>
+      <div class="bordered-box">
+        <div class="group">
+          <div class="options">
+            <h4>Apertura</h4>
+            <div class="hour">
+              <input type="time" v-model="openTime" />
+            </div>
+            <h4>Cierre</h4>
+            <div class="hour">
+              <input type="time" v-model="closeTime" />
             </div>
           </div>
-          <div class="last-box">
-            <div class="box-image">
-              <div>
-                <input type="text" v-model="image1" class="field" placeholder="Imagen 1 (URL)" />
-              </div>
-              <div>
-                <input type="text" v-model="image2" class="field" placeholder="Imagen 2 (URL)" />
-              </div>
+          <div class="options">
+            <h4>Equipamiento</h4>
+            <div>
+              <label for="furniture">
+                <input type="checkbox" v-model="furniture" />Muebles
+              </label>
             </div>
-            <textarea
-              v-model="description"
-              placeholder="Descripción de la sala"
-              class="field"
-              rows="5"
-              cols="30"
-            ></textarea>
-            <div class="center">
-              <!-- BOTÓN PARA ENVIAR LOS DATOS -->
-              <button @click="editSpace()">Enviar Datos</button>
-              <button @click="cancel()">Cancelar</button>
+            <div>
+              <label for="wifi">
+                <input type="checkbox" v-model="wifi" />WI-FI
+              </label>
+            </div>
+            <div>
+              <label for="projector">
+                <input type="checkbox" v-model="projector" />Proyector
+              </label>
+            </div>
+            <div>
+              <label for="screen">
+                <input type="checkbox" v-model="screen" />Pantalla
+              </label>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="last-box">
+        <div class="box-image">
+          <textarea
+            v-model="description"
+            placeholder="Descripción de la sala"
+            class="field"
+            rows="5"
+            cols="30"
+          ></textarea>
+          <div>
+            <input
+              type="file"
+              @change="onFileSelected"
+              ref="images"
+              name="spaces"
+              id="img"
+              multiple
+            />
+          </div>
+        </div>
+        <div>
+          <!-- BOTÓN PARA ENVIAR LOS DATOS -->
+          <button @click="editSpace()">Enviar Datos</button>
+          <button @click="cancel()">Cancelar</button>
         </div>
       </div>
     </div>
@@ -138,6 +155,7 @@ import {
   getSpaceById,
   getSpaceId,
   updateSpace,
+  saveImage,
 } from "./../../../backend/requests/space";
 import { getUserId, getAuthToken } from "./../../../backend/requests/user";
 export default {
@@ -163,6 +181,8 @@ export default {
       screen: 0,
       image1: "",
       image2: "",
+      newImage1: null,
+      newImage2: null,
       description: "",
     };
   },
@@ -196,6 +216,12 @@ export default {
       this.projector = space.projector;
       this.image1 = space.image_1;
       this.image2 = space.image_2;
+      this.newImage1 = null;
+      this.newImage2 = null;
+    },
+    onFileSelected() {
+      this.newImage1 = this.$refs.images.files[0];
+      this.newImage2 = this.$refs.images.files[1];
     },
     validateSpace() {
       if (
@@ -206,8 +232,6 @@ export default {
         !this.price ||
         !this.openTime ||
         !this.closeTime ||
-        !this.image1 ||
-        !this.image2 ||
         !this.description
       ) {
         Swal.fire("No puedes dejar campos vacíos");
@@ -224,6 +248,12 @@ export default {
       let token = getAuthToken();
       let open = this.openTime.slice(0, 2);
       let close = this.closeTime.slice(0, 2);
+      if (this.newImage1) {
+        this.image1 = await saveImage(this.newImage1);
+      }
+      if (this.newImage2) {
+        this.image2 = await saveImage(this.newImage2);
+      }
       let space = {
         hotel: this.hotel,
         city: this.city,
@@ -245,6 +275,7 @@ export default {
         image2: this.image2,
         description: this.description,
       };
+
       if (await updateSpace(token, this.id, space)) {
         Swal.fire("Espacio actualizado");
         this.$router.push({ name: "SpaceProviderList" });
@@ -263,15 +294,9 @@ export default {
 </script>
 
 <style scoped>
-
-#main {
-    background: linear-gradient(white,#2c3e50);
-}
-
-h3 {
-  margin-bottom: 20px;
-}
 .container {
+  background: linear-gradient(white, #2c3e50);
+  width: 100%;
   display: flex;
   justify-content: center;
 }
@@ -284,6 +309,10 @@ h3 {
   margin-bottom: 20px;
   background-color: #ccc;
   color: #2c3e50;
+}
+
+h3 {
+  margin-bottom: 20px;
 }
 
 .group {
@@ -301,6 +330,10 @@ h3 {
   border-top: 1px solid lightgray;
   margin-top: 25px;
   margin-bottom: 20px;
+}
+
+#img {
+  width: 310px;
 }
 
 .last-box {
@@ -357,6 +390,10 @@ textarea {
   background-color: #f6f6f6;
 }
 
+textarea {
+  width: 310px;
+}
+
 h4 {
   padding-left: 45px;
 }
@@ -372,5 +409,4 @@ input {
 label {
   padding-left: 16px;
 }
-
 </style>
